e75a21c3e0d90bd7de20d61ca14cd2f769c5e92a
agus <agusgc_99@hotmail.com>
2020-02-12 18:41:11 -0300

    Auto stash before revert of "dependencias-fuentes"

--- yoga/src/app/app.module.ts
+++ yoga/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { FlexLayoutModule} from '@angular/flex-layout'
+
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
