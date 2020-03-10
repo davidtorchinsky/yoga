3d7751d3e9bf91805d6faa27db0a2786cfdbe591
agus <agusgc_99@hotmail.com>
2020-03-10 17:39:12 -0300

    efectos header

--- yoga/src/app/sidebar/sidebar.component.ts
+++ yoga/src/app/sidebar/sidebar.component.ts
@@ -1,17 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  animations: [
+
+    trigger('gatillo', [
+      // ...
+      state('abierto', style({
+
+        width: '*',
+        opacity: 1,
+
+      })),
+      state('cerrado', style({
+
+        width: '0px',
+        opacity: 0,
+     
+      })),
+      transition('abierto => cerrado', [
+        animate('0.75s')
+      ]),
+      transition('cerrado => abierto', [
+        animate('0.75s')
+      ]),
+    ]),
+  ]
 })
 export class SidebarComponent implements OnInit {
 
   constructor() { }
 
+  private sidebar:boolean=false;
+  private check=false;
+
   ngOnInit(): void {
 
     
+  }
+
+  click(){
+    this.sidebar=!this.sidebar;
+    this.check=!this.check;
+    console.log("sidebar "+this.sidebar+" check "+this.check)
   }
 
 }
