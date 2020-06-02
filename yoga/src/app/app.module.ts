abf63eb9d45200c65989dc77a6bf0ba59917792e
Agus <agusgc_99@hotmail.com>
2020-06-02 16:16:23 -0300

    side bar Responsive

--- yoga/src/app/app.module.ts
+++ yoga/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import { FlexLayoutModule} from '@angular/flex-layout'
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FlexLayoutModule} from '@angular/flex-layout';
 
 
 
@@ -12,28 +12,28 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { LoginComponent } from './login/login.component';
 
-//Rutas
+// Rutas
 import {APP_ROUTING} from './app.routes';
 
 
-//Servicios
-//para el login
-import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
-import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+// Servicios
+// para el login
+import { SocialLoginModule, AuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider, FacebookLoginProvider } from 'angularx-social-login';
 
- 
- 
-let config = new AuthServiceConfig([
+
+
+const config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
-    provider: new GoogleLoginProvider("380939380894-mfd67bl4r70blmgur8r7odje5csm0b16.apps.googleusercontent.com")
+    provider: new GoogleLoginProvider('380939380894-mfd67bl4r70blmgur8r7odje5csm0b16.apps.googleusercontent.com')
   }/* ,
   {
     id: FacebookLoginProvider.PROVIDER_ID,
     provider: new FacebookLoginProvider("Facebook-App-Id")
   } */
 ]);
- 
+
 export function provideConfig() {
   return config;
 }
@@ -44,7 +44,7 @@ export function provideConfig() {
     HeaderComponent,
     SidebarComponent,
     ContactoComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
